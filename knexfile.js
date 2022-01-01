88e73d947a13c447cd81d7405056fa7e65a5eba1
Boris P <pustovetov.boris@yandex.ru>
2022-01-02 01:54:47 +0300

    step 4

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,7 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     useNullAsDefault: true,
-    searchPath: ['knex', 'public'],
+    ssl: { rejectUnauthorized: false },
     migrations,
   },
 };
