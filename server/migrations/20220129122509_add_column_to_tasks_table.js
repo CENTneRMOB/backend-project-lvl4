27310754b97302a9b705456917100fca78505f34
Boris P <pustovetov.boris@yandex.ru>
2022-01-29 12:54:39 +0300

    knex migration changed

--- server/migrations/20220129122509_add_column_to_tasks_table.js
+++ server/migrations/20220129122509_add_column_to_tasks_table.js
@@ -2,7 +2,7 @@
 
 exports.up = (knex) => (
   knex.schema.table('tasks', (table) => {
-    table.specificType('labels_id', 'int[]').references('id').inTable('labels');
+    table.integer('labels_id').references('id').inTable('labels');
   })
 );
 
