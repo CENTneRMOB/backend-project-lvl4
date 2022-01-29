a331434476e87635d9c8129f0c2178ca40b92c16
Boris P <pustovetov.boris@yandex.ru>
2022-01-29 12:41:18 +0300

    knex migration changed

--- server/migrations/20220105204859_create_tasks_table.js
+++ server/migrations/20220105204859_create_tasks_table.js
@@ -8,7 +8,7 @@ exports.up = (knex) => (
     table.integer('status_id').references('id').inTable('statuses');
     table.integer('creator_id').references('id').inTable('users');
     table.integer('executor_id').nullable().references('id').inTable('users');
-    table.specificType('labels_id', 'int[]').references('id').inTable('labels');
+    // table.specificType('labels_id', 'int[]').references('id').inTable('labels');
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
   })
